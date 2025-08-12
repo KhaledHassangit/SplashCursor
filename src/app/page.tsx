3d7e8de179201d814466c921d20e6b0064298b83
Khaled Hassan <132917415+KhaledHassangit@users.noreply.github.com>
2025-08-12 17:36:30 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import LightRays from "./components/NavBar";
+import SplashCursor from "./components/SplashCursor";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#00ffff"
+          raysSpeed={1.5}
+          lightSpread={0.8}
+          rayLength={1.2}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0.1}
+          distortion={0.05}
+          className="custom-rays"
+        />
+      </div>
+      <SplashCursor />
+    </div>
+  );
+}
